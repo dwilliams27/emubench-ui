60ec57742ea48818830c0b16e7afd413607a3e24
Farmerbilly27 <dmw2796@gmail.com>
2025-08-20 20:32:27 -0500

    wip goal

---
+++ src/components/test/active/active-test-goal.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export function ActiveTestGoal() {
+  return (
+    <Card className="w-full md:w-1/3">
+      <CardHeader>
+        <CardTitle>Goal</CardTitle>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+    </Card>
+  )
+}
