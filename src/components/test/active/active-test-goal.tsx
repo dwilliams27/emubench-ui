7eb9560f043f746aa6f889dbc9a5a3647a287f15
Farmerbilly27 <dmw2796@gmail.com>
2025-08-26 20:00:39 -0500

    wip goal component

--- src/components/test/active/active-test-goal.tsx
+++ src/components/test/active/active-test-goal.tsx
@@ -1,13 +1,38 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { EmuConditionOperand } from "@/shared/conditions/types";
 
-export function ActiveTestGoal() {
+function renderOperand(operand: EmuConditionOperand) {
+  if (operand.conditionPart) {
+    return (
+      <div>
+        {operand.conditionPart.operation.name}
+      </div>
+    );
+  } else if (operand.input) {
+    return (
+      <div>
+        {operand.input.name}: {operand.input.parsedValue}
+      </div>
+    );
+  } else if (operand.primitive) {
+    return (
+      <div>
+        {operand.primitive}
+      </div>
+    )
+  }
+}
+
+export function ActiveTestGoal({ flatCondition }: { flatCondition: EmuConditionOperand[] }) {
   return (
     <Card className="w-full md:w-1/3">
       <CardHeader>
         <CardTitle>Goal</CardTitle>
       </CardHeader>
       <CardContent>
-        
+        {
+          flatCondition.map(operand => renderOperand(operand))
+        }
       </CardContent>
     </Card>
   )
